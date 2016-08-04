86dc8c0bfa20e947a647d04c113ae7cc825f5434
wishinghand <wishinghand@gmail.com>
2016-08-03 17:22:45 -0700

    adding comments

--- index.js
+++ index.js
@@ -14,18 +14,18 @@ $('#calculateBtn').click(function(){
     //number of payments
     var paymentsNum = term * period;
 
-    //
+    //turns the APR into a proper percentage, then divides by the (bi)monthly period
     var monthlyInterestRate = (apr / 100) / period;
 
-    //
+    //raises the interest rate+1 by the paymentsNum
     var compoundedInterestRate = Math.pow((1 + monthlyInterestRate), paymentsNum);
 
-    //
+    //more math I could barely follow
     var interestQuotient  = (monthlyInterestRate * compoundedInterestRate) / (compoundedInterestRate - 1);
 
-    //
+    //Making number integer, but could still have float due to dividing by 100
     var monthlyPayment = Math.round((balance * interestQuotient) * 100) / 100;
 
-    //Put monthly payment value in a string on page
+    //Put monthly payment value in a string on page, and making sure all results have two decimal places
     $('#output').text("Your monthly payment will be $" + monthlyPayment.toFixed(2));
 });
